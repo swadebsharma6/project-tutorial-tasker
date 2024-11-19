23fd12c93d96afe0c44765198eb1b6f6770bf1c2
SwadebDeb sharma <swadebriva2012@gmail.com>
2024-11-20 01:16:33 +0600

    getting an error in addTaskModal components

--- src/components/Task/TaskBoard.jsx
+++ src/components/Task/TaskBoard.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskAction from "./TaskAction";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
 
 const TaskBoard = () => {
+      const defaultTask ={
+            'id': crypto.randomUUID(),
+            'title': 'Learn React js',
+            'description': 'I want to be a full stack web developer,, Best learning platform is lear with sumit',
+            'tags': ['Web','JavaScript', 'React'],
+            'priority': 'High',
+            'isFavorite': true
+
+      }
+      const [tusks, setTusks] = useState([defaultTask]);
+      const [showAddModal, setShowModal] = useState(false);
+
+      const handleAddTask =(task)=>{
+            console.log('btn is clicked', task)
+      }
+
   return (
     <section className="mb-20" id="tasks">
+      { showAddModal && <AddTaskModal onSave={handleAddTask}/> }
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchTask/>
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskAction/>
-         <TaskList/>
+          <TaskAction onAddClick={()=>setShowModal(true)}/>
+          <TaskList tusks={tusks}/>
         </div>
       </div>
     </section>
